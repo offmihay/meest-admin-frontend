ce058f48a6310bd19d503f7f7c4bb9bfd6e23bb1
Volodymyr <offmihay@gmail.com>
2024-05-12 18:13:46 +0300

    changed url api

--- src/components/hooks/Api.tsx
+++ src/components/hooks/Api.tsx
@@ -1,5 +1,5 @@
-// const BASE_URL: string = "https://meest-back-e18db4f449d7.herokuapp.com";
-const BASE_URL: string = "http://localhost:3000/api";
+const BASE_URL: string = "https://meest-back-e18db4f449d7.herokuapp.com";
+// const BASE_URL: string = "http://localhost:3000/api";
 
 export async function fetchJson(url: string, token?: string): Promise<any> {
   const headers: Record<string, string> = {
